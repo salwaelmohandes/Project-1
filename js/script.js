736b8aa81f9d07c210c5ed3c634d37d7486acc2a
salwaelmohandes <60805227+salwaelmohandes@users.noreply.github.com>
2020-02-10 09:56:09 -0500

    create random quote function
    grab the random quote object from the array of quotes and store it in a variable then return the variable with the random quote

--- js/script.js
+++ js/script.js
@@ -12,22 +12,51 @@ project 1 - A Random Quote Generator
 ***/
 
 
+const quotes = [
+  {
+    quote : "Nothing bring people together like good food.",
+    source: "Anonymous ",
+  },
+  {
+    quote : "It's amazing how a little tomorrow can make up for a whole lot of yesterday.",
+    source: "John Guare",
+    citation:"Landscape Body Play"
+  },
+  {
+    quote : "The best way to predict the future is to create it.",
+    source: "Abraham Lincoln"
+  },
+  {
+    quote : "Try to be rainbow in someone's cloud.",
+    source: "Maya Angelou",
+    year: 2008
+  },
+  {
+    quote : "Never ruin an apology with an excuse.",
+    source: "Benjamin Franklin"
+  }];
+  
+  
+  /***
+   * `getRandomQuote` function
+  ***/
+  
+  function getRandomQuote() {
+    let randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+    return randomQuote;
+  }
+  
+  
+  /***
+   * `printQuote` function
+  ***/
+ 
+  
+  
 
-/***
- * `getRandomQuote` function
-***/
-
-
-
-/***
- * `printQuote` function
-***/
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
-
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+  /***
+   * click event listener for the print quote button
+   * DO NOT CHANGE THE CODE BELOW!!
+  ***/
+  
+  document.getElementById('load-quote').addEventListener("click", printQuote, false);
